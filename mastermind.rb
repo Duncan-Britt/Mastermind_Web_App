5247b44c5b4496526e518d38ad05c82df919ff2c
Duncan Britt <dbru997@gmail.com>
2021-09-07 21:52:58 -0600

    Client side input validation and submission with AJAX

--- mastermind.rb
+++ mastermind.rb
@@ -13,12 +13,49 @@ get '/' do
   erb :home, layout: :layout
 end
 
-get '/play/code_breaker' do
+class InvalidRangeError < StandardError; end
+
+def valid_range?(range)
+  range.max <= 9 && range.min >= -9
+end
+
+def random_code(code_size: 4, digit_range: (1..6))
+  unless valid_range?(digit_range)
+    raise InvalidRangeError.new(
+      'Digit range must be a range containing single digit integers only'
+    )
+  end
+
+  code = []
+  code_size.times { code << rand(digit_range.size) + digit_range.min }
+  code
+end
+
+def new_client_setup
+  session[:code_size] ||= 4
+  session[:digit_range] ||= (1..6)
   session[:turn] ||= 1
+  session[:code] ||= random_code(
+    code_size: session[:code_size],
+    digit_range: session[:digit_range]
+  )
+end
+
+get '/play/code_breaker' do
+  new_client_setup
 
   erb :code_breaker, layout: :layout
 end
 
 post '/play/code_breaker' do
   session[:turn] += 1
+
+  guess = params[:guess].chars.map { |n| Integer(n) } # [int, int, int, int]
+  p guess
+
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else
+    redirect '/play/code_breaker'
+  end
 end
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,59 @@
+let form = document.querySelector('#code-break');
+const CODESIZE = form.dataset.codeSize;
+const MIN = form.dataset.min;
+const MAX = form.dataset.max;
 
+function convertGuess(guess) {
+  digits = guess.split('').map(chr => parseInt(chr))
+  return digits
+};
+
+// digits must either be passed in or be defined in scope
+function validateRange(digits) {
+  if (digits.length != CODESIZE) {
+    return false
+  };
+
+  validity = true
+  digits.forEach((item, i) => {
+    if (item < MIN || item > MAX) {
+      validity = false
+    };
+  });
+
+  return validity
+}
+
+$(function() {
+  $("form#code-break").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let data = $('form#code-break').serialize()
+    let input = data.match(/\d+/)[0]
+    let digits = convertGuess(input)
+
+    if (validateRange(digits)) {
+      console.log("ran")
+
+      form = $(this)
+      console.log(form)
+
+      var request = $.ajax({
+        url: form.attr("action"),
+        method: form.attr("method"),
+        data: $("input#guess")
+      })
+
+      request.done(function(data, textStatus, jqXHR) {
+        if (jqXHR.status === 204) {
+
+        } else if (jqXHR.status === 200) {
+
+        }
+      })
+    } else {
+      console.log("Invalid input") // PUT FLASH MESSAGE HERE
+    }
+  })
+});
